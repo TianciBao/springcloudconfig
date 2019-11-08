a47ec65231ac2c7d9604b0eabc7027e234bbc3e8
Tianci Bao <tiabao@cisco.com>
2019-11-08 16:05:54 +0800

    test3

--- src/main/java/com/cisco/webex/tpgw/controller/LocusRestController.java
+++ src/main/java/com/cisco/webex/tpgw/controller/LocusRestController.java
@@ -386,9 +386,7 @@ public class LocusRestController {
     public static void main(String[] args) throws IOException {
        Integer xx =  909588537;
        System.out.println(xx.byteValue());
-       
-       
-       
+      
    }
 
 }
